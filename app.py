a5338b1ecd7c683a9e172a0e0b00195ad935f6df
utkar-2018 <deokar.pritee@gmail.com>
2025-03-01 13:38:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def predict():
     return jsonify({"disease": prediction[0]})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
 
